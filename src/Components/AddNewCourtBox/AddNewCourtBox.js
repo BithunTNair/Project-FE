867de87ac8bd1bc5b3a77eef88be343e51599195
BithunT-Nair <react666cr@gmail.com>
2024-05-12 21:50:29 +0530

    newcourt changes

--- src/Components/AddNewCourtBox/AddNewCourtBox.js
+++ src/Components/AddNewCourtBox/AddNewCourtBox.js
@@ -1,24 +1,48 @@
 import React, { useRef, useState } from 'react'
 import './AddNewCourtBox.css'
 import Input from '../Common/InputField/Input'
+import addIcon from '@Assets/plus-add-new-create-svgrepo-com.svg'
+import ReactQuill from 'react-quill';
+import 'react-quill/dist/quill.snow.css';
+import AxiosInstance from '../../Config/apicall';
 
 
 
 function AddNewCourtBox() {
     const [courtData, setCourtData] = useState({})
     const fileInputRef = useRef()
-    const [selectedFiles,setSelectedFiles]=useState([])
+    const [selectedFiles, setSelectedFiles] = useState([])
     const handleChange = (e) => {
         setCourtData({ ...courtData, [e.target.name]: e.target.value })
     }
     const handleInputFileChange = (e) => {
         const files = Array.from(e.target.files);
-        const validFiles= files.filter((file)=>{return file.type.startsWith('image/') || file.type.startsWith('video/')})
-        setSelectedFiles(prevState=>[...prevState,...validFiles])
+        const validFiles = files.filter((file) => { return file.type.startsWith('image/') || file.type.startsWith('video/') })
+        setSelectedFiles((prevState) => [...prevState, ...validFiles])
     }
 
     const handleAddIcon = () => {
         fileInputRef.current.click()
+    }
+    const handleDescriptionChange=(data)=>{
+        setCourtData({...courtData,description:data})
+    }
+    const handleCreateCourt=()=>{
+
+const formDatatoSend= FormData()
+selectedFiles.forEach((file)=>{
+    formDatatoSend.append('files',file)
+})
+Object.entries(courtData).forEach(([key,value])=>{
+    formDatatoSend.append(key,value)
+})
+
+        AxiosInstance({
+            url:'/admin/createnewcourt',
+            method:'POST',
+            data:formDatatoSend
+
+        })
     }
 
     return (
@@ -29,7 +53,7 @@ function AddNewCourtBox() {
                     <h3>Add New Court</h3>
                     <span className='d-flex gap-3'>
                         <button className='common-btn bg-black text-white'> {" "} Cancel</button>
-                        <button className='common-btn'>Create</button>
+                        <button className='common-btn' onClick={handleCreateCourt}> {" "} Create</button>
                     </span>
                 </div>
                 <div className='col-lg-4 col-md-6 mt-3 '>
@@ -59,18 +83,36 @@ function AddNewCourtBox() {
                 <div className='col-lg-4 col-md-6 mt-3 '>
                     <Input type={'number'} name={'contactNumber'} label={'Contact Number'} value={courtData.contactNumber} onchange={handleChange} />
                 </div>
-                <div className='mt-3'>
-                    {setSelectedFiles.map((file,index)=>
-                    <>
-                    {}
-                    </>
-                    )}
-                   <div>
-                   <input type="file" ref={fileInputRef} onchange={handleInputFileChange} multiple accept='image/*,video/*' style={{ display: 'none' }} />
-                    <img src="" alt="" srcset="" height={'150px'} onClick={handleAddIcon} />
-                   </div>
+                <div className="mt-3 d-flex flex-wrap gap-2">
+                    {selectedFiles.map((file, index) => (
+                        <>
+                            {file.type.startsWith("image/") && (
+                                <img src={URL.createObjectURL(file)} height={150} key={index} />
+                            )}
+                            {file.type.startsWith("video/") && (
+                                <video src={URL.createObjectURL(file)} height={150} key={index} />
+                            )}
+                        </>
+                    ))}
+                    <div>
+                        <input
+                            type="file"
+                            ref={fileInputRef}
+                            onChange={handleInputFileChange}
+                            multiple
+                            accept="image/*, video/*"
+                            style={{ display: "none" }}
+                        />
+                        <img
+                            src={addIcon}
+                            alt=""
+                            width={"50px"}
+                            height={"50px"}
+                            onClick={handleAddIcon}
+                        />
+                    </div>
                 </div>
-
+                <ReactQuill className='my-3' style={{height:'300px'}} theme="snow" value={courtData.description} onChange={handleDescriptionChange} />
             </div>
 
 
